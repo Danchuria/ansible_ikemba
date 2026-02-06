f5a20daf0a66064bc4a88636b662f90e229bbbbf
danchuria <danchuria@alu.edu.gva.es>
2026-02-06 18:04:17 +0100

    Añadir app Node.js completa

--- app/index.js
+++ app/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola desde Node desplegado con Ansible y GitHub Actions');
+  res.send('¡Práctica Ansible CI/CD funcionando!');
 });
 
 app.listen(port, () => {
-  console.log(`App escuchando en puerto ${port}`);
+  console.log(`App en puerto ${port}`);
 });
